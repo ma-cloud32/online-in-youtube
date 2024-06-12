f3216383b6bc320a359158ff2c2d813794a48942
ma-cloud32 <mdarshad78508@gmail.com>
2024-06-12 15:49:11 +0000

    comparison

---
+++ 03-datatypes.js
@@ -0,0 +1,40 @@
+// Primitive data type
+// note: call by value
+// 7 types : string ,Number, Boolean,null,undefined,Symbol,
+BigInt
+const score=100
+const scorValue =100.3
+const isLoggedIn= false
+const outsidTemp = null
+let userEmail= undefined
+//let userEmail;
+const id =Symbol('123')
+const anotherId = Symbol('123') 
+console.log(id===anotherId)
+const i4 = 23
+const i3 = 23
+console.log(i4===i3)
+const bigNumber=938888993399999n
+
+
+// Reference (Non primitive)
+// Array ,Objects,funtions
+const heros=["Ahetesham","Intekhab","Inzamam"]
+let myObj ={
+    name:"Arshad",
+    age:22,
+}
+ 
+//  const myFuntion = funtion(){
+ //  console.log("hello world");
+ //}
+   //console.log(typeof score)
+  // console.log(typeof outsideTemp)
+   console.log(typeof heros)
+
+
+
+ 
+
+
+ 
