3ade63733846a8c8840141a168137522f90f31a0
ma-cloud32 <mdarshad78508@gmail.com>
2024-06-12 06:16:33 +0000

    EMCA

---
+++ 02-datatypes.js
@@ -0,0 +1,28 @@
+console.log(3+3)
+let age = 23
+console.log(age)
+ // number => to power 53
+ // bigint
+ // string=>""
+ // boolean=> true\false
+ // null => standalone value
+ // undefined=>
+// symbol=> unique
+
+
+ // object 
+ console.log(typeof("Md Arshad"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
