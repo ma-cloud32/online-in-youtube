a9ec0de07caaa90ddb3bf236504c6ae679ef1733
ma-cloud32 <mdarshad78508@gmail.com>
2024-06-12 14:48:47 +0000

    comparison

---
+++ comparison.js
@@ -0,0 +1,13 @@
+// console.log(2>1)
+// console.log(2==2)
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+console.log()
+
+console.log(undefined==0)
+// note this type of conversion to avoid this situation
+
+// ===
+console.log("2"===2)
+
