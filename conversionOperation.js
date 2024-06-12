66d9cef4a3293c0b0a6e9a41ffcf5a5e277e1487
ma-cloud32 <mdarshad78508@gmail.com>
2024-06-12 14:37:22 +0000

    conversion operation

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,27 +1,63 @@
-//let score ="hitesh"
+// // //let score ="hitesh"
 
-//console.log(typeof score)
-//console.log(typeof(score))
-//let valueInNumber = Number(score)
-//console.log(typeof valueInNumber)
-//console.log(valueInNumber)
-
-
-// "33"=> 33
-// "33abc"=> NaN
-// true=1 false=0;
-
-let isLoggedIn ="8"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// // //console.log(typeof score)
+// // //console.log(typeof(score))
+// // //let valueInNumber = Number(score)
+// // //console.log(typeof valueInNumber)
+// // //console.log(valueInNumber)
 
 
-// 1=> true; 0=false
-// ""=> false
-// "hitesh"= true
-let someNumber =33
+// // // "33"=> 33
+// // // "33abc"=> NaN
+// // // true=1 false=0;
 
-let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// // let isLoggedIn ="8"
+// // let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // //console.log(booleanIsLoggedIn);
+
+
+// // // 1=> true; 0=false
+// // // ""=> false
+// // // "hitesh"= true
+// // let someNumber =33
+
+// // let stringNumber =String(someNumber)
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber)
+    
+
+
+// // //  ***************** Operations *************
+
+// // //let negValue= -value
+// // //console.log(negValue)
+// // console.log(2+2)
+// // console.log(2-2)
+// // console.log(2*2)
+// // console.log(2**2)
+// // console.log(2/2)
+// // console.log(2%2)
+// // console.log(2+2)
+ 
+ let str1 ="hello"
+ let str2 = " Arshad"
+let str3 = str1 + str2
+// console.log(str3)
+// console.log("1"+2)
+// console.log(2+"3")
+// console.log("1"+"3")
+// console.log("1"+2 +"2")
+// console.log("1"+2)
+// console.log((3+4)*5 %3)
+ 
+console.log(+true)
+console.log(+"")
+ 
+let num1 , num2, num3
+num1 =num2=num3=2+2
+ 
+let gameCounter = 100
+
+++gameCounter
+console.log(gameCounter)
 
