5bdeea4e756e3486756a4a8395b61e5d3b7f705f
ma-cloud32 <mdarshad78508@gmail.com>
2024-06-15 19:53:53 +0000

    Array

---
+++ Array.js
@@ -0,0 +1,27 @@
+//  let marvelHeroes =["thor","spiderman", "ironman"];
+// let dcHeroes = ["superman", "batman"];
+ //et heroes = marvelHeroes.concat(dcHeroes);
+//console.log(heroes);
+
+
+  //let val= marvelHeroes.unshift("Md Arshad")
+  //console.log(val)
+
+
+// let closeFriends =["Ashfaque","Atif","Rafi"];
+// let bestFriends =["Aftab","shahid","Azad","Rabbani"]
+// let friends = closeFriends.concat(bestFriends);
+
+// console.log(friends);
+
+//  console.log(marvelHeroes);
+
+// console.log(marvelHeroes.slice())
+
+// let marvelHeroes =["thor","spiderman", "ironman","antman","Dr stranger"]
+
+
+// splice()
+ 
+let arr = [1,2,3,4,5,6,7];
+ console.log(arr.splice(2,2,98,89))
