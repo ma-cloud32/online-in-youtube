e14dab5520c12297c1f40ca48376fbc62ec29a3b
ma-cloud32 <mdarshad78508@gmail.com>
2024-06-19 13:48:44 +0000

    function

--- funtion.js
+++ funtion.js
@@ -24,26 +24,67 @@
 //     return x*y;
 //  }
 
-// Arrow function
- const arrowSum=(a,b)=>{
-    console.log(a+b); 
- }
+// // Arrow function
+//  const arrowSum=(a,b)=>{
+//     console.log(a+b); 
+//  }
 
- const arrowMul =(a,b)=>{
-    console.log(a*b);
- }
+//  const arrowMul =(a,b)=>{
+//     console.log(a*b);
+//  }
   
- function countVowels(str){
-    let count =0;
-    for(const char of str){
-       if(char ==="e"|| char ==="a"|| char ==="o"|| char ==="u"|| char ==="i") {
-        count++;
-       }
-     }
-     console.log(count);
- }
- countVowels("education");
+//  function countVowels(str){
+//     let count =0;
+//     for(const char of str){
+//        if(char ==="e"|| char ==="a"|| char ==="o"|| char ==="u"|| char ==="i") {
+//         count++;
+//        }
+//      }
+//      console.log(count);
+//  }
+//  countVowels("education");
  
  
-//  myFunction("string");
- // calculate the vowel
+// //  myFunction("string");
+//  // calculate the vowel
+
+// //  let arr  = [5,6,2,1,101,3];
+  
+// //  const output = arr.reduce((prev,curr)=>{
+// //     return  prev > curr?prev : curr;
+// //  });
+
+// //  console.log(output);
+
+// //   5>6 = 6
+
+
+//  // let's practice
+//  // Question 1
+ 
+// //  let arr = [98,76,93,74,91,92];
+
+// //   let Arr  = arr.filter((val) =>{
+// //    return val > 90;
+// //   });
+
+// //   console.log(Arr);
+
+// // Qusetion 02
+
+ let  arr = [1,2,3,4,5,6,7,8,9,10];
+
+let sum = arr.reduce((res, curr)=>{
+   return res + curr;
+});
+
+console.log("sum = ", sum);
+
+let fact = arr.reduce((res,curr)=>{
+   return res*curr;
+});
+
+console.log("fact" ,fact);
+
+
+
